df4ad554ae35e09ff8eb4e9d2164245a823eebf6
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-09-25 15:25:46 -0400

    Display library cards with JS, adjust HTML nesting

--- script.js
+++ script.js
@@ -14,9 +14,46 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(newBook); // add Book instance to myLibrary
 }
 
+function displayLibrary(myLibrary) {
+  myLibrary.forEach(book => { // iterate over books in library
+    // create title, author, pages, and buttons elements; add classes
+    const title = document.createElement('h3');
+    title.textContent = book.title;
+    title.classList.add('title');
+
+    const author = document.createElement('p');
+    author.textContent = book.author;
+    author.classList.add('author');
+
+    const pages = document.createElement('p');
+    pages.textContent = `${book.pages} pages`;
+    pages.classList.add('pages');
+    
+    const read = document.createElement('button');
+    read.textContent = book.read == true ? 'Read' : 'Not Read';
+    const remove = document.createElement('button');
+    remove.textContent = 'Remove';
+    const buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+    [read, remove].forEach(el => buttons.appendChild(el));
+    
+    // create a new card
+    const card = document.createElement('div');
+    card.classList.add('card');
+    [title, author, pages, buttons].forEach(el => card.appendChild(el));
+
+    // add card to main-content
+    const main = document.querySelector('.main-content');
+    main.appendChild(card);
+  });
+}
+
+
 addBookToLibrary('Brave New World', 'Aldous Huxley', 288, true);
 addBookToLibrary('The Brothers Karamazov', 'Fyodor Dostoevsky', 840);
 addBookToLibrary('Walden', 'Henry David Thoreau', 311);
 addBookToLibrary('The Eye of the World', 'Robert Jordan', 812, true);
 
 console.log(myLibrary);
+
+displayLibrary(myLibrary);
