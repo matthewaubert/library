f1f7571b488ac3db3970e7e6dff14552b3fa0504
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-09-25 21:12:17 -0400

    Enable user to submit new book

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ It is a simple library application in which a user can add books to their collec
 The main focus of the app should be the cards – one card for each book added. There will be a button that allows the user to add a new book. This button will pull up a form contained within a modal dialog. Upon submission of the form, the user input will be taken and placed within a new card and added to the grid of cards.
 
 The user input should be stored behind the scenes in an array of objects – one object per book, containing all relevant information.
+
+// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295);
--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const myLibrary = [];
 
+enableModalDialog();
+enableSubmitBook();
+
+// Book constructor function
 function Book(title, author, pages, read = false) {
   this.title = title;
   this.author = author;
@@ -7,49 +11,49 @@ function Book(title, author, pages, read = false) {
   this.read = read;
 }
 
-// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295);
-
+// create new Book object instance and add to myLibrary
 function addBookToLibrary(title, author, pages, read) {
-  let newBook = new Book(title, author, pages, read); // create new Book object instance
-  myLibrary.push(newBook); // add Book instance to myLibrary
+  let newBook = new Book(title, author, pages, read); 
+  myLibrary.push(newBook);
 }
 
-function displayLibrary(myLibrary) {
-  myLibrary.forEach(book => { // iterate over books in library
-    // create title, author, pages, and buttons elements; add classes
-    const title = document.createElement('h3');
-    title.textContent = book.title;
-    // title.classList.add('title');
-
-    const author = document.createElement('p');
-    author.textContent = book.author;
-    // author.classList.add('author');
-
-    const pages = document.createElement('p');
-    pages.textContent = `${book.pages} pages`;
-    // pages.classList.add('pages');
-    
-    const read = document.createElement('button');
-    read.textContent = book.read == true ? 'Read' : 'Not Read';
-    const remove = document.createElement('button');
-    remove.textContent = 'Remove';
-    const buttons = document.createElement('div');
-    buttons.classList.add('buttons');
-    [read, remove].forEach(el => buttons.appendChild(el));
-    
-    // create a new card
-    const card = document.createElement('div');
-    card.classList.add('card');
-    [title, author, pages, buttons].forEach(el => card.appendChild(el));
-
-    // add card to main-content
-    const main = document.querySelector('.main-content');
-    main.appendChild(card);
-  });
+// iterate over Book instances in library and display them
+function displayLibrary(library) {
+  library.forEach(book => displayBook(book));
 }
 
-function enableAddBook() {
-  // add event listener to add button
+// display Book instance as card on screen
+function displayBook(book) {
+  // create title, author, pages, and buttons elements; add classes
+  const title = document.createElement('h3');
+  title.textContent = book.title;
+
+  const author = document.createElement('p');
+  author.textContent = book.author;
+
+  const pages = document.createElement('p');
+  pages.textContent = `${book.pages} pages`;
+  
+  const read = document.createElement('button');
+  read.textContent = book.read == true ? 'Read' : 'Not Read';
+  const remove = document.createElement('button');
+  remove.textContent = 'Remove';
+  const buttons = document.createElement('div');
+  buttons.classList.add('buttons');
+  [read, remove].forEach(el => buttons.appendChild(el));
+  
+  // create a new card, add content
+  const card = document.createElement('div');
+  card.classList.add('card');
+  [title, author, pages, buttons].forEach(el => card.appendChild(el));
+
+  // add card to main-content
+  const main = document.querySelector('.main-content');
+  main.appendChild(card);
+}
+
+// enable buttons to open and close the modal dialog with form
+function enableModalDialog() {
   const addBook = document.querySelector('.add-book');
   const dialog = document.querySelector('#dialog');
   addBook.addEventListener('click', () => {
@@ -59,7 +63,6 @@ function enableAddBook() {
 
   const cancel = document.querySelector('#cancel');
   cancel.addEventListener('click', () => {
-    // close modal
     dialog.close();
     openCheck(dialog);
   });
@@ -73,22 +76,40 @@ function enableAddBook() {
   }
 }
 
+// enable the submit button in the modal dialog to submit a new book
+function enableSubmitBook() {
+  // select form inputs
+  const form = document.querySelector('form');
+  const title = document.querySelector('#title');
+  const author = document.querySelector('#author');
+  const pages = document.querySelector('#pages');
+  const read = document.querySelector('#read');
+  const submit = document.querySelector('#submit');
+  
+  // add click event listener to submit button
+  submit.addEventListener('click', () => {
+    console.log(title.value, author.value, pages.value, read.checked);
+    addBookToLibrary(title.value, author.value, pages.value, read.checked);
+    displayBook(myLibrary[myLibrary.length - 1]);
+    form.reset();
+  });
+}
+
 
 addBookToLibrary('Brave New World', 'Aldous Huxley', 288, true);
 addBookToLibrary('The Brothers Karamazov', 'Fyodor Dostoevsky', 840);
 addBookToLibrary('Walden', 'Henry David Thoreau', 311);
 addBookToLibrary('The Eye of the World', 'Robert Jordan', 812, true);
-addBookToLibrary('Brave New World', 'Aldous Huxley', 288, true);
-addBookToLibrary('The Brothers Karamazov', 'Fyodor Dostoevsky', 840);
-addBookToLibrary('Walden', 'Henry David Thoreau', 311);
-addBookToLibrary('The Eye of the World', 'Robert Jordan', 812, true);
-addBookToLibrary('Brave New World', 'Aldous Huxley', 288, true);
-addBookToLibrary('The Brothers Karamazov', 'Fyodor Dostoevsky', 840);
-addBookToLibrary('Walden', 'Henry David Thoreau', 311);
-addBookToLibrary('The Eye of the World', 'Robert Jordan', 812, true);
+// addBookToLibrary('Brave New World', 'Aldous Huxley', 288, true);
+// addBookToLibrary('The Brothers Karamazov', 'Fyodor Dostoevsky', 840);
+// addBookToLibrary('Walden', 'Henry David Thoreau', 311);
+// addBookToLibrary('The Eye of the World', 'Robert Jordan', 812, true);
+// addBookToLibrary('Brave New World', 'Aldous Huxley', 288, true);
+// addBookToLibrary('The Brothers Karamazov', 'Fyodor Dostoevsky', 840);
+// addBookToLibrary('Walden', 'Henry David Thoreau', 311);
+// addBookToLibrary('The Eye of the World', 'Robert Jordan', 812, true);
 
 console.log(myLibrary);
 
 displayLibrary(myLibrary);
 
-enableAddBook();
